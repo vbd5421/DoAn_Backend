1730ba43f2dc6726f100ae1ac268cfe8065375b0
@vdb5421 <duongvb@ecoit.com.vn>
2023-10-13 16:42:12 +0700

    create

---
+++ src/main/java/com/doan/backend/DoAnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.doan.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doan/backend/DoAnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
