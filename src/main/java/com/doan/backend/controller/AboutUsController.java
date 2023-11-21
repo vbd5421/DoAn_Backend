77f4044ebeae792f6cd83d6f3d1278d12ed84295
@vdb5421 <duongvb@ecoit.com.vn>
2023-11-21 22:34:19 +0700

    Update AboutUsController.java

--- src/main/java/com/doan/backend/controller/AboutUsController.java
+++ src/main/java/com/doan/backend/controller/AboutUsController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/about")
+@RequestMapping("/api/about")
 public class AboutUsController {
     @Autowired
     private AboutUsService aboutUsService;
